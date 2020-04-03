4c4e5b12554c1db6b36333696f89d3d49bfe24d6
Amruth Pillai <im.amruth@gmail.com>
2020-04-03 14:44:20 +0530

    Updates

---
+++ precache-manifest.a2b977578354ec2cd6a4dbf889fb2cd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db97345cd683e7b02b440646a086e8a2",
+    "url": "/React-Tailwind-Starter/index.html"
+  },
+  {
+    "revision": "668c78ae4da643cc1847",
+    "url": "/React-Tailwind-Starter/static/css/main.5949dc8a.chunk.css"
+  },
+  {
+    "revision": "408724866542e12a89a4",
+    "url": "/React-Tailwind-Starter/static/js/2.b1a30adc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Tailwind-Starter/static/js/2.b1a30adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668c78ae4da643cc1847",
+    "url": "/React-Tailwind-Starter/static/js/main.5d960619.chunk.js"
+  },
+  {
+    "revision": "bd7d3343d27d05e5884f",
+    "url": "/React-Tailwind-Starter/static/js/runtime-main.173ce761.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f8c3328398796b3010fcbed0187b4b5.js"
+  "/React-Tailwind-Starter/precache-manifest.a2b977578354ec2cd6a4dbf889fb2cd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Tailwind-Starter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
