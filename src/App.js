ddd5558c7638814409a417c2e56d7c9072560ca1
Amruth Pillai <im.amruth@gmail.com>
2020-04-03 14:13:56 +0530

    add tailwind, eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <div className="shadow my-12 py-12 flex justify-center items-center">
+        <h1 className="text-xl font-medium">
+          Thank you for using the React Tailwind Starter project!
+        </h1>
+      </div>
     </div>
   );
 }
